cddcf51f645073eaa04907026f2e23854f474029
Tymivv <if210683tvn@gmail.com>
2024-10-13 17:52:35 +0300

    size file

--- src/components/files/Files.jsx
+++ src/components/files/Files.jsx
@@ -1,21 +1,47 @@
-import style from './files.module.css';
+import React from 'react';
+import style from '../app.module.css';
 
-const Files = ({files, removeFile, moveFile}) =>{
-    return (
+
+const Files = ({ fileInfos, removeFile, moveFile }) => {
+return (
     <ul className={style.preview}>
-    {files?.map((file, index) => (
-        <li className={style.list}key={index}>
-            {file?.name}{' '}
-            <div className={style.previewRemove} onClick={() => removeFile(index)}>&#10006;</div>
+    {fileInfos?.map((info, index) => (
+        <li className={style.list} key={index}>
+        <div className={style.fileName}>
+            <strong>{info.file.name}</strong> 
+            <div className={style.sizePsges}>
+                Розмір сторінок: {info.maxWidth.toFixed(0)} x {info.maxHeight.toFixed(0)}
+            </div>
+        </div>
+        <div className={style.controls}>
+            <div
+                className={style.previewRemove}
+                onClick={() => removeFile(index)} // Викликаємо функцію видалення файлу
+                >
+                    &#10006; 
+            </div>
             {index > 0 && (
-            <div className={style.arrow_up} onClick={() => moveFile(index, index - 1)}>&#11014;</div>
+            <div
+                className={style.arrow_up}
+                onClick={() => moveFile(index, index - 1)} // Переміщуємо файл вгору
+                >
+                &#11014; 
+            </div>
             )}
-            {index < files.length - 1 && (
-            <div className={style.arrow_down} onClick={() => moveFile(index, index + 1)}>&#11015;</div>
+            {index < fileInfos.length - 1 && (
+            <div
+                className={style.arrow_down}
+                onClick={() => moveFile(index, index + 1)} // Переміщуємо файл вниз
+                >
+                &#11015; 
+            </div>
             )}
+        </div>
+            
         </li>
     ))}
     </ul>
-);
+    );
 };
+
 export default Files;
